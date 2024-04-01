345c0d47f68f77faf3271211fbc8c9149766c686
LimeNade <alexandre.zarkua@gmail.com>
2024-04-01 18:47:55 +0200

    Add inputs
    Added number input and image input

---
+++ src/lib/utils/BlockGen/Inputs/NumberInput.ts
@@ -0,0 +1,35 @@
+import BaseInput from "./BaseInput";
+
+
+export default class NumberInput extends BaseInput {
+    private readonly _name: string
+    private readonly _value: number
+    private readonly _settings: {
+        "min"?: number,
+        "max"?: number,
+        "precision"?: number
+    }
+
+    constructor (name: string, defaultValue: number, settings?: {"min"?: number,"max"?: number,"precision"?: number} ) {
+        super()
+
+        this.setMethod(this.getDefinition)
+        this._name = name
+        this._value = defaultValue
+        this._settings = settings || {}
+    }
+
+    private getDefinition(): {"type": "field_number","name": string,"value": number,} {
+        return {
+            type: "field_number",
+            name: this._name,
+            value: this._value,
+            ...this._settings
+        }
+    }
+
+    get name(): string {
+        return this._name
+    }
+
+}
