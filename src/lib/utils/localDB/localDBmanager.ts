9f271295f3cae9a019046f5772456183a9798eb0
LimeNade <alexandre.zarkua@gmail.com>
2024-04-03 20:10:33 +0200

    Chore: Lint

--- src/lib/utils/localDB/localDBmanager.ts
+++ src/lib/utils/localDB/localDBmanager.ts
@@ -1,35 +1,46 @@
 import { Dexie, type Table } from "dexie";
 
 export default class LocalDBManager extends Dexie {
-    plugins!: Table<DiscodesPluginDataStore, string>
-    users!: Table<User, string>
+	plugins!: Table<DiscodesPluginDataStore, string>;
+	users!: Table<User, string>;
 
-    constructor() {
-        super("DiscodesDatabase")
+	constructor() {
+		super("DiscodesDatabase");
 
-        this.version(1).stores({
-            plugins: "id",
-            users: "id"
-        })
-    }
+		this.version(1).stores({
+			plugins: "id",
+			users: "id"
+		});
+	}
 
-    async getPlugin(id: string) {
-        const val = await this.plugins.get(id)
+	/**
+	 * Returns the plugin table or returns undefined if the owner or the plugin doesn't exist.
+	 *
+	 * @param {string} id
+	 * @return {*}  {(Promise<DiscodesPlugin | undefined>)}
+	 * @memberof LocalDBManager
+	 */
+	async getPlugin(id: string): Promise<DiscodesPlugin | undefined> {
+		const val = await this.plugins.get(id);
 
-        if(!val) return undefined
+		if (!val) return undefined;
 
-        const ownerId = val.owner
-        const owner = await this.users.get(ownerId)
+		const ownerId = val.owner;
+		const owner = await this.users.get(ownerId);
 
-        if(!owner) return undefined
+		if (!owner) return undefined;
 
-        const obj: DiscodesPlugin = {
-            ...val,
-            owner
-        }
+		const obj: DiscodesPlugin = {
+			...val,
+			owner
+		};
 
-        return obj
-    }
+		return obj;
+	}
+
+	async createUser(userData: User) {
+		await this.users.add(userData, window.crypto.randomUUID());
+	}
 }
 
 //TODO Make those interfaces!
@@ -37,63 +48,63 @@ export default class LocalDBManager extends Dexie {
 type BlockConfig = unknown;
 
 type DiscodesPluginDataStore = {
-    id: string,
-    name: string
-    owner: string
-    description: string
-    downloads: number
-    likes: number
-    rating: number
-    version: number
-    blocks: BlockConfig[]
-}
+	id: string;
+	name: string;
+	owner: string;
+	description: string;
+	downloads: number;
+	likes: number;
+	rating: number;
+	version: number;
+	blocks: BlockConfig[];
+};
 
 type DiscodesPlugin = {
-    id: string,
-    owner: User
-    name: string
-    description: string
-    downloads: number
-    likes: number
-    rating: number
-    version: number
-    blocks: BlockConfig[]
-}
+	id: string;
+	owner: User;
+	name: string;
+	description: string;
+	downloads: number;
+	likes: number;
+	rating: number;
+	version: number;
+	blocks: BlockConfig[];
+};
 
 type User = {
-    username: string
-    id: string
-    follows: User[]
-    followers: User[]
-    createdAt: Date
-    workspaces: DiscodesWorkspace[]
-    publishedPlugins: DiscodesPlugin[]
-    publishedExamples: DiscodesFile[]
-}
+	username: string;
+	id: string;
+	follows: User[];
+	followers: User[];
+	createdAt: Date;
+	workspaces: DiscodesWorkspace[];
+	publishedPlugins: DiscodesPlugin[];
+	publishedExamples: DiscodesFile[];
+};
 
 type BlocklyWorkspaceSave = {
-    workspaceSave: object | string
-    blockLength: number
-}
+	workspaceSave: object | string;
+	blockLength: number;
+};
 
 type DiscodesFile = {
-    name: string
-    createdAt: Date
-    lastEditedAt: Date
-    blocklyWorkspaceSave: BlocklyWorkspaceSave
-    thumbnail: string
-    timeWasted: number
-}
+	name: string;
+	createdAt: Date;
+	lastEditedAt: Date;
+	blocklyWorkspaceSave: BlocklyWorkspaceSave;
+	thumbnail: string;
+	timeWasted: number;
+};
 
 type DiscodesWorkspace = {
-    files: DiscodesFile[]
-    createdAt: Date
-    lastEditedAt: Date
-    owner: string
-    editors: string[]
-    viewers: string[]
-    id: string
-    name: string
-    description: string
-    timeWasted: number
-}
+	files: DiscodesFile[];
+	createdAt: Date;
+	lastEditedAt: Date;
+	owner: string;
+	editors: string[];
+	viewers: string[];
+	id: string;
+	name: string;
+	description: string;
+	timeWasted: number;
+};
