c79454efd149596521ce9fa0500a08365331670d
Retro <htoomyataung382@gmail.com>
2024-04-07 14:11:32 +0630

    📝 Clean up code and update localDB

--- src/lib/utils/localDB/localDBmanager.ts
+++ src/lib/utils/localDB/localDBmanager.ts
@@ -1,52 +1,145 @@
 import { Dexie, type Table } from "dexie";
 
-export default class LocalDBManager extends Dexie {
-    plugins!: Table<DiscodesPluginDataStore, string>
-    users!: Table<User, string>
+let dbManager: LocalDBManager
+
+export function getDB() {
+    if (!dbManager) {
+        dbManager = new LocalDBManager()
+    }
+
+    return dbManager
+}
+
+// SINGLETON CLASS! DO NOT BUILD
+class LocalDBManager extends Dexie {
+    plugins!: Table<DiscodesPlugin, string>
+    workspaces!: Table<DiscodesWorkspace, string>
+    examples!: Table<DiscodesWorkspace, string>
 
     constructor() {
         super("DiscodesDatabase")
 
         this.version(1).stores({
             plugins: "id",
-            users: "id"
+            workspaces: "id",
+            examples: "id",
         })
     }
+}
 
-    async getPlugin(id: string) {
-        const val = await this.plugins.get(id)
+let userStore: UserStorage
 
-        if(!val) return undefined
+export function getUserStore() {
+    if (!userStore) userStore = new UserStorage()
 
-        const ownerId = val.owner
-        const owner = await this.users.get(ownerId)
+    return userStore
+}
 
-        if(!owner) return undefined
+// SINGLETON CLASS! DO NOT BUILD
+class UserStorage {
+    // Be sure to implement both push and pull methods inside these 3 methods (i.e. Pushing offline created ones to server and pulling online ones from the server)
+    async syncPlugins(): Promise<DiscodesPlugin> {
+        throw new Error('Method not implemented')
+    }
 
-        const obj: DiscodesPlugin = {
-            ...val,
-            owner
-        }
+    async syncWorkspaces(): Promise<DiscodesWorkspace> {
+        throw new Error('Method not implemented')
+    }
 
-        return obj
+    async syncExamples(): Promise<DiscodesWorkspace> {
+        throw new Error('Method not implemented')
+    }
+
+    setUser(user: User) {
+        localStorage.setItem("userdata", JSON.stringify(user))
+    }
+
+    async refreshFollowers(): Promise<string[]> {
+        throw new Error("Method not implemented") // TODO: Dynamically fetch followers based on their id/the user id from the database
+    }
+
+    async refreshFollowings(): Promise<string[]> {
+        throw new Error("Menthod not implemented")
+    }
+
+    async getFollowers() {
+        await this.refreshFollowers()
+
+        const userData = this.getUserData()
+
+        return await Promise.all(userData.followers.map(v => new UserDataGetter(v)))
+    }
+
+    async getFollowings() {
+        await this.refreshFollowings()
+
+        const userData = this.getUserData()
+
+        return await Promise.all(userData.follows.map(v => new UserDataGetter(v)))
+    }
+
+    async getPublishedExamples() {
+        const userDataJson = this.getUserData()
+
+        const db = getDB()
+
+        await this.syncExamples()
+
+        const plugins = await Promise.all(userDataJson.publishedExamples.map((str) => db.plugins.get(str)))
+
+        return plugins
+    }
+
+    getUserData() {
+        const userData = localStorage.getItem("userdata")
+
+        if (!userData) throw new Error("User has not logged on yet")
+
+        const userDataJson = JSON.parse(userData) as User // I don't like using 'as' here but I've got no choice
+
+        return userDataJson
+    }
+
+    async getPublishedPlugins() {
+        const userDataJson = this.getUserData()
+
+        const db = getDB()
+
+        await this.syncPlugins()
+
+        const plugins = await Promise.all(userDataJson.publishedPlugins.map((str) => db.plugins.get(str)))
+
+        return plugins
+    }
+
+    async getWorkspaces() {
+        const userData = this.getUserData()
+
+        const db = getDB()
+
+        await this.syncWorkspaces()
+
+        const workspaces = await Promise.all(userData.workspaces.map((str) => db.workspaces.get(str)))
+
+        return workspaces
+    }
+}
+
+export class UserDataGetter {
+    id: string
+
+    constructor(id: string) {
+        this.id = id
+    }
+
+    async getData(): Promise<User> {
+        throw new Error("Method not implemented")
     }
 }
 
 //TODO Make those interfaces!
 //? Some data are for the backend only, if you feel like it doesn't belong in the localDB don't add them! Thx <3
 type BlockConfig = unknown;
-
-type DiscodesPluginDataStore = {
-    id: string,
-    name: string
-    owner: string
-    description: string
-    downloads: number
-    likes: number
-    rating: number
-    version: number
-    blocks: BlockConfig[]
-}
 
 type DiscodesPlugin = {
     id: string,
@@ -63,12 +156,12 @@ type DiscodesPlugin = {
 type User = {
     username: string
     id: string
-    follows: User[]
-    followers: User[]
+    follows: string[]
+    followers: string[]
     createdAt: Date
-    workspaces: DiscodesWorkspace[]
-    publishedPlugins: DiscodesPlugin[]
-    publishedExamples: DiscodesFile[]
+    workspaces: string[]
+    publishedPlugins: string[]
+    publishedExamples: string[]
 }
 
 type BlocklyWorkspaceSave = {
