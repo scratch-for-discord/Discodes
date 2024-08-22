e6bfbba33a6272b1eeef6078cedd29fedee1bcc4
Retro <htoomyataung382@gmail.com>
2024-08-22 21:07:28 +0630

    Implement hide navbar feature

--- src/lib/components/EditorNavbar.svelte
+++ src/lib/components/EditorNavbar.svelte
@@ -9,32 +9,45 @@
 	// import { showNavbar } from "$lib/stores/navbarStore";
 	// import Page from "../../routes/editor/+page.svelte";
 
+	let isShowing = true
+
 	const dispatch = createEventDispatcher();
 </script>
 
-<div class="absolute top-4 w-full z-20 pr-5" style={`padding-left: ${toolBoxWidth + 20}px`}>
+<div class={`absolute top-4 ${isShowing ? "w-full" : "w-[15%] justify-center flex items-center"} transition-all z-20 pr-5`} style={`padding-left: ${toolBoxWidth + 20}px`}>
 	<div
-		class="w-full bg-background p-1 px-2 rounded-md shadow-xl border-gray-200 dark:border-neutral-700 py-2"
+		class="w-full bg-background flex p-1 px-2 rounded-md shadow-xl border-gray-200 dark:border-neutral-700 py-2"
 	>
+		{#if isShowing}
+			<Button
+				variant="outline"
+				class="h-8 mx-1"
+				on:click={() => {
+					dispatch("save");
+				}}>SAVE</Button
+			>
+			<Button
+				variant="outline"
+				class="h-8 mx-1"
+				on:click={() => {
+					dispatch("load");
+				}}
+			>
+				LOAD
+			</Button>
+			<Button variant="link" class="h-8 mx-1" href='/'>
+				Home
+			</Button>
+			<Warnings bind:workspace />
+		{/if}
 		<Button
-			variant="outline"
-			class="h-8"
+			variant="default"
+			class="h-8 ml-auto flex items-center"
 			on:click={() => {
-				dispatch("save");
-			}}>SAVE</Button
-		>
-		<Button
-			variant="outline"
-			class="h-8"
-			on:click={() => {
-				dispatch("load");
+				isShowing = !isShowing
 			}}
 		>
-			LOAD
+			{isShowing ? "<" : ">"}
 		</Button>
-		<Button variant="link" class="h-8" href='/'>
-			Home
-		</Button>
-		<Warnings bind:workspace />
 	</div>
 </div>
