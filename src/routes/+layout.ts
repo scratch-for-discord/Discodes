db7182c574ccfdd57e6c0e1abf60cae3ed847e9b
Noxyyk <setunskyto@gmail.com>
2024-09-28 15:16:37 +0200

    better login pages, 404 page (#66)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,16 @@
 export const ssr = false;
+
+import { redirect } from '@sveltejs/kit';
+import getLocalDB from '$lib/utils/localDB/manager';
+
+const loginLock = ['/interface'];
+//redirect to login in locked pages
+export const load = async ({ url }) => {
+	const db = getLocalDB();
+	if (loginLock.includes(url?.pathname) && !db.userID) {
+		throw redirect(302, '/auth/login');
+	}
+	return {
+		user: db?.userID || null
+	};
+};
